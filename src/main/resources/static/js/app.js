74023a29867a8251122a3f6f3d589a5d33b8cfd9
ubinah60-cyber <ubinah60@gmail.com>
2026-06-11 16:24:44 +0900

    feat: add music search page ui

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,37 @@
+document.getElementById("searchBtn")
+    .addEventListener("click", searchMusic);
+
+async function searchMusic() {
+
+    const keyword =
+        document.getElementById("keyword").value;
+
+    const response =
+        await fetch(`/api/music/search?keyword=${keyword}`);
+
+    const musicList =
+        await response.json();
+
+    renderMusicList(musicList);
+}
+
+function renderMusicList(musicList) {
+
+    const resultArea =
+        document.getElementById("resultArea");
+
+    resultArea.innerHTML = "";
+
+    musicList.forEach(music => {
+
+        resultArea.innerHTML += `
+            <div>
+                <h3>${music.title}</h3>
+                <p>가수 : ${music.artist}</p>
+                <p>앨범 : ${music.album}</p>
+                <p>장르 : ${music.genre}</p>
+                <hr>
+            </div>
+        `;
+    });
+}
